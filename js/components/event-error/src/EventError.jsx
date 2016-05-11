cbfece24fe9f6c683ae18454b5db74397559c842
Savva <cabehok@inbox.ru>
2016-05-11 20:11:02 +0300

    change error view

--- js/components/event-error/src/EventError.jsx
+++ js/components/event-error/src/EventError.jsx
@@ -4,23 +4,22 @@ import Config from '../config';
 class EventError extends React.Component {
 
 	static propTypes = {
-		error: React.PropTypes.string.isRequired
+		error: React.PropTypes.string.isRequired,
+		className: React.PropTypes.string
 	}
 
 	render () {
 		return (
-			<div className="event-info-box">
-				<section className="event-info">
-					<div className="event-info__header">
+			<div className="event-error-box">
+				<section className="event-error">
+					<div className="event-error__header">
 						<a href="#calendar" className="close-btn">&times;</a>
 					</div>
-					<div className="event-info__body">
-						<img className="error-img" src={Config.img.eventNotFound}/>
-						<div className="error-text">
-							<strong>оО...</strong>
+					<div className="event-error__body">
+						<img className="event-error__error-img"/>
+						<div className="event-error__error-text">
 						 	<span>{this.props.error}</span>
 						 	<span>Попробуйте выбрать другое мероприятие.</span>
-						 	<h1>404</h1>
 						</div>
 					</div>
 				</section>
--- js/config.js
+++ js/config.js
@@ -75,10 +75,6 @@ module.exports = {
 		modalId: 'modal'
 	},
 
-	img: {
-		eventNotFound: '/react/events/build/icons/event_not_found.png'
-	},
-
 	hashes: {
 		calendar: 'calendar',
 		eventView: 'event/view/{id}',
